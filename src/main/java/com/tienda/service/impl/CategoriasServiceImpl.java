61ecc01cc6da4b7390c626bc7714773c28f00f5b
Isaac Ruiz <isaacruizse@gmail.com>
2025-02-18 21:26:48 -0600

    semana 6

--- src/main/java/com/tienda/service/impl/CategoriasServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriasServiceImpl.java
@@ -14,13 +14,31 @@ public class CategoriasServiceImpl implements CategoriaService {
     @Autowired
     private CategoriaDao categoriaDao;
 
-    
-    public List<Categoria> getCategorias(boolean activos){
+    @Override
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
         var lista = categoriaDao.findAll();
-        
-        if(activos){ //quiere solo las categorias activas
-            lista.removeIf(c -> !c.isActivo());
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.deleteById(categoria.getIdCategoria());
     }
 }
